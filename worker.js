add5cef441f757c8118535bc1cc8c97705db725b
rainysome <69748159+rainysome@users.noreply.github.com>
2020-08-30 20:40:05 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,21 +1,24 @@
-self.importScripts("./source.js");
-/*
+//self.importScripts("./source.js");
+
 function worker_function()
 {
+	/*
 	console.log("hi");
 	self.onmessage = event =>
 	{
 		alert("hell");
 		df = event.data["func"];
 		Function(df + "();");
-	}	
+	}
+	*/
 }
 
 if (window != self)
 	worker_function();
-*/
+/*
 self.onmessage = event =>
 {
 	df = event.data["func"];
 	Function(df + "();");
 }	
+*/
